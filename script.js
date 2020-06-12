24d96accaa53e8ec084b4e4dd9663844a7acef1f
Vasyl-007 <vasyl.manyo@gmail.com>
2020-06-12 23:59:18 +0300

    added all directions

--- script.js
+++ script.js
@@ -127,4 +127,13 @@ window.addEventListener("keydown", function (e) {
   if (e.keyCode == 37) {
     console.log("left");
   }
+  if (e.keyCode == 38) {
+    console.log("up");
+  }
+  if (e.keyCode == 39) {
+    console.log("right");
+  }
+  if (e.keyCode == 40) {
+    console.log("down");
+  }
 });
