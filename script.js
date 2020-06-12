1db7fd1452227593ca2e2c7dbee91fbaaa334962
Vasyl-007 <vasyl.manyo@gmail.com>
2020-06-12 22:25:02 +0300

    added functions to the snake game

--- script.js
+++ script.js
@@ -24,3 +24,52 @@ for (let i = 0; i < excel.length; i++) {
   x++;
   //   y++;
 }
+
+function generateSnake() {
+  let posX = Math.round(Math.random() * (10 - 3) + 3);
+  let posY = Math.round(Math.random() * (10 - 1) + 1);
+  return [posX, posY];
+}
+
+let coordinates = generateSnake();
+// console.log(coordinates);
+// console.log(coordinates[0]);
+// console.log(coordinates[1]);
+let snakeBody = [
+  document.querySelector(
+    '[posX = "' + coordinates[0] + '"][posY = "' + coordinates[1] + '"]'
+  ),
+  document.querySelector(
+    '[posX = "' + (coordinates[0] - 1) + '"][posY = "' + coordinates[1] + '"]'
+  ),
+  document.querySelector(
+    '[posX = "' + (coordinates[0] - 2) + '"][posY = "' + coordinates[1] + '"]'
+  ),
+];
+
+for (let i = 0; i < snakeBody.length; i++) {
+  snakeBody[i].classList.add("snakeBody");
+}
+snakeBody[0].classList.add("head");
+
+// console.log(coordinates);
+console.log(snakeBody);
+
+let fruit;
+function createFruit() {
+  function generateFruit() {
+    let posX = Math.round(Math.random() * (10 - 1) + 1);
+    let posY = Math.round(Math.random() * (10 - 1) + 1);
+    return [posX, posY];
+  }
+  let fruitCoordinates = generateFruit();
+  console.log("fruitCoordinates", fruitCoordinates);
+  fruit = document.querySelector(
+    '[posX = "' +
+      fruitCoordinates[0] +
+      '"][posY = "' +
+      fruitCoordinates[1] +
+      '"]'
+  );
+}
+createFruit();
