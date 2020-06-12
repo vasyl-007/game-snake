51c057a44017261bc1530c971a292bb626fefe6e
Vasyl-007 <vasyl.manyo@gmail.com>
2020-06-13 00:37:54 +0300

    added function eating berries

--- script.js
+++ script.js
@@ -98,22 +98,92 @@ function move() {
   snakeBody[snakeBody.length - 1].classList.remove("snakeBody");
   snakeBody.pop();
 
-  if (snakeCoordinates[0] < 10) {
-    snakeBody.unshift(
-      document.querySelector(
-        '[posX = "' +
-          (+snakeCoordinates[0] + 1) +
-          '"][posY = "' +
-          snakeCoordinates[1] +
-          '"]'
-      )
+  if (direction === "right") {
+    if (snakeCoordinates[0] < 10) {
+      snakeBody.unshift(
+        document.querySelector(
+          '[posX = "' +
+            (+snakeCoordinates[0] + 1) +
+            '"][posY = "' +
+            snakeCoordinates[1] +
+            '"]'
+        )
+      );
+    } else {
+      snakeBody.unshift(
+        document.querySelector(
+          '[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'
+        )
+      );
+    }
+  } else if (direction === "left") {
+    if (snakeCoordinates[0] > 1) {
+      snakeBody.unshift(
+        document.querySelector(
+          '[posX = "' +
+            (+snakeCoordinates[0] - 1) +
+            '"][posY = "' +
+            snakeCoordinates[1] +
+            '"]'
+        )
+      );
+    } else {
+      snakeBody.unshift(
+        document.querySelector(
+          '[posX = "10"][posY = "' + snakeCoordinates[1] + '"]'
+        )
+      );
+    }
+  } else if (direction === "up") {
+    if (snakeCoordinates[1] < 10) {
+      snakeBody.unshift(
+        document.querySelector(
+          '[posX = "' +
+            snakeCoordinates[0] +
+            '"][posY = "' +
+            (+snakeCoordinates[1] + 1) +
+            '"]'
+        )
+      );
+    } else {
+      snakeBody.unshift(
+        document.querySelector(
+          '[posX = "' + snakeCoordinates[0] + '"][posY = "1"]'
+        )
+      );
+    }
+  } else if (direction === "down") {
+    if (snakeCoordinates[1] > 1) {
+      snakeBody.unshift(
+        document.querySelector(
+          '[posX = "' +
+            snakeCoordinates[0] +
+            '"][posY = "' +
+            (+snakeCoordinates[1] - 1) +
+            '"]'
+        )
+      );
+    } else {
+      snakeBody.unshift(
+        document.querySelector(
+          '[posX = "' + snakeCoordinates[0] + '"][posY = "10"]'
+        )
+      );
+    }
+  }
+
+  if (
+    snakeBody[0].getAttribute("posX") === fruit.getAttribute("posX") &&
+    snakeBody[0].getAttribute("posY") === fruit.getAttribute("posY")
+  ) {
+    // console.log("Match - is true!");
+    fruit.classList.remove("fruit");
+    let a = snakeBody[snakeBody.length - 1].getAttribute("posX");
+    let b = snakeBody[snakeBody.length - 1].getAttribute("posY");
+    snakeBody.push(
+      document.querySelector('[posX = "' + a + '"][posY = "' + b + '"]')
     );
-  } else {
-    snakeBody.unshift(
-      document.querySelector(
-        '[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'
-      )
-    );
+    createFruit();
   }
 
   snakeBody[0].classList.add("head");
@@ -121,19 +191,24 @@ function move() {
     snakeBody[i].classList.add("snakeBody");
   }
 }
+
 // let interval = setInterval(move, 300)
 
 window.addEventListener("keydown", function (e) {
-  if (e.keyCode == 37) {
-    console.log("left");
+  if (e.keyCode === 37 && direction !== "right") {
+    // console.log("left");
+    direction = "left";
   }
-  if (e.keyCode == 38) {
-    console.log("up");
+  if (e.keyCode === 38 && direction !== "down") {
+    // console.log("up");
+    direction = "up";
   }
-  if (e.keyCode == 39) {
-    console.log("right");
+  if (e.keyCode === 39 && direction !== "left") {
+    // console.log("right");
+    direction = "right";
   }
-  if (e.keyCode == 40) {
-    console.log("down");
+  if (e.keyCode === 40 && direction !== "up") {
+    // console.log("down");
+    direction = "down";
   }
 });
